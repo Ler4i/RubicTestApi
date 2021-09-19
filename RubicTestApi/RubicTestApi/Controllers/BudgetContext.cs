be1e077255f7facd248d4b603dc2dc980adfbeb9
Ler4i <valeriazotova553@gmail.com>
2021-09-19 17:53:22 +0300

    Мы создали сервер

---
+++ RubicTestApi/RubicTestApi/Controllers/BudgetContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RubicTestApi.Models;
+
+namespace RubicTestApi.Controllers
+{
+    public class BudgetContext : DbContext
+    {
+        public BudgetContext(DbContextOptions<BudgetContext> options)
+            : base(options)
+        { 
+
+        }
+        public DbSet<Budget> Budgets { get; set; }//будет "бегать" по БД
+    }
+}
